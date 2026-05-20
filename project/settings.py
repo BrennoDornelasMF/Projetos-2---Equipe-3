7545aa7fccfb4e362e2802c6ffea2d5beb8a8842
BrennoDornelasMF <bdmf@cesar.school>
2026-05-20 18:53:21 -0300

    fix

--- project/settings.py
+++ project/settings.py
@@ -134,8 +134,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media' 
+
+ALLOWED_HOSTS = ['project.onrender.com', 'localhost']
+
+DEBUG = False
 
