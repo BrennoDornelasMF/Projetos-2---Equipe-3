b8cf4f323c2e7bb56e3ffeeb1114568ec57fbe9b
BrennoDornelasMF <bdmf@cesar.school>
2026-06-10 18:16:51 -0300

    debug migrations

--- usuarios/migrations/0002_create_superuser.py
+++ usuarios/migrations/0002_create_superuser.py
@@ -9,13 +9,20 @@ def create_superuser(apps, schema_editor):
     email = os.environ.get('SUPERUSER_EMAIL', 'admin@admin.com')
     password = os.environ.get('SUPERUSER_PASSWORD')
 
+    print(f">>> SUPERUSER_USERNAME: {username}")
+    print(f">>> SUPERUSER_EMAIL: {email}")
+    print(f">>> SUPERUSER_PASSWORD existe: {bool(password)}")
+
     if password:
         User.objects.filter(username=username).delete()
-        User.objects.create_superuser(
+        user = User.objects.create_superuser(
             username=username,
             email=email,
             password=password
         )
+        print(f">>> Superuser criado: {user.username} | is_staff: {user.is_staff} | is_superuser: {user.is_superuser}")
+    else:
+        print(">>> ERRO: SUPERUSER_PASSWORD não encontrada nas variáveis de ambiente!")
 
 class Migration(migrations.Migration):
 
