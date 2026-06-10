18dfe062bb681e3e9b95183d8660edf30b872f0c
BrennoDornelasMF <bdmf@cesar.school>
2026-06-10 17:36:46 -0300

    create root user_admin

---
+++ usuarios/migrations/0002_create_superuser.py
@@ -0,0 +1,27 @@
+from django.db import migrations
+import os
+
+def create_superuser(apps, schema_editor):
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+
+    username = os.environ.get('SUPERUSER_USERNAME', 'root')
+    email = os.environ.get('SUPERUSER_EMAIL', 'root@admin.com')
+    password = os.environ.get('SUPERUSER_PASSWORD')
+
+    if password and not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('usuarios', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
