db4e2bb0b8a3bd33162f79d39e2660c1edd08f71
BrennoDornelasMF <bdmf@cesar.school>
2026-06-15 17:01:53 -0300

    fix img path

--- project/urls.py
+++ project/urls.py
@@ -32,8 +32,9 @@ urlpatterns = [
     path('patrocinios/', include('patrocinios.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
+
