851947159858b567360b3b98127be9fa0f07eb43
BrennoDornelasMF <bdmf@cesar.school>
2026-06-16 11:40:41 -0300

    fix MIDDLEWARE

--- project/settings.py
+++ project/settings.py
@@ -21,8 +21,13 @@ ALLOWED_HOSTS = [
 AUTH_USER_MODEL = 'usuarios.Usuario'
 
 INSTALLED_APPS = [
-    'cloudinary_storage',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
     'dashboard',
     'emprestimos',
@@ -31,11 +36,6 @@ INSTALLED_APPS = [
     'usuarios',
     'core',
     'forum',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'patrocinios',
 ]
 
